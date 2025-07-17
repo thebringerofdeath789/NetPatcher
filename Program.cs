9fccc41ae9313437b24d8d8d057c3f1ffe12867b
thebringerofdeath789 <thebringerofdeath789@gmail.com>
2025-07-17 09:51:57 -0700

    Update Program.cs
    deleted a tab on line 2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 /* Author		: Gregory King
- * Date			: 07/16/25
+ * Date		: 07/16/25
  * Description	: This program is a .NET patcher that allows users to modify .NET applications to execute specific actions. The program will find compatible binaries in the Program Files, Program Files (x86) or user profile directories, and then applies the specified patch based on user input. The patched executable will be copied to the current directory with a new name. This version uses dnlib for binary manipulation. The patched program will execute the injected payload immediately when started. After the payload, the original program logic resumes, unless the payload itself interrupts or terminates execution (for example, if the payload throws an exception or calls Environment.Exit).
  * 
  * Patching options:
